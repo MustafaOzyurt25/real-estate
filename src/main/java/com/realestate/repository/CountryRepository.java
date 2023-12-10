ebd6df2bb135081aba46b956e5253e1ad9aed786
Alifuatakcn <fakcan4908@gmail.com>
2023-12-10 22:38:02 +0300

    first commit

---
+++ src/main/java/com/realestate/repository/CountryRepository.java
@@ -0,0 +1,9 @@
+package com.realestate.repository;
+
+import com.realestate.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CountryRepository extends JpaRepository <Country,Long> {
+
+}
---
+++ src/main/java/com/realestate/service/CountryService.java
@@ -0,0 +1,12 @@
+package com.realestate.service;
+
+import com.realestate.repository.CountryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CountryService {
+
+    private final CountryRepository countryRepository;
+}
