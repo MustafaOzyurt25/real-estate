ad6bd3b39e50f9036f68f40c5fc2e8daf0d60a76
mikzeus <beymelok@gmail.com>
2023-12-11 10:19:06 +0300

    advertType

---
+++ src/main/java/com/realestate/payload/response/AdvertTypeResponse.java
@@ -0,0 +1,17 @@
+package com.realestate.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class AdvertTypeResponse {
+
+    private Long id;
+    private String title;
+
+}
