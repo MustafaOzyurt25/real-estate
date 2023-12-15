9aba390e40c0e4af2a616fb591bdf93677fd4338
MustafaOzyurt25 <131266012+MustafaOzyurt25@users.noreply.github.com>
2023-12-15 19:09:04 +0300

    update and get methods for categorypropertykey

--- src/main/java/com/realestate/service/CategoryPropertyKeyService.java
+++ src/main/java/com/realestate/service/CategoryPropertyKeyService.java
@@ -112,8 +112,7 @@ public class CategoryPropertyKeyService {
     }
 
 
-    // -----------------------------------------------------------------------------------------------------------------------------------------------  
-
+    
 
     public CategoryPropertyKey isCategoryPropertyKeyExistById(Long id) {
 
