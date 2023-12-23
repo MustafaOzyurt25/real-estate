edecf585e0f0fab92fda66dc74e30e9863a0afd8
Alifuatakcn <fakcan4908@gmail.com>
2023-12-23 21:58:28 +0300

    update-city_district

--- src/main/java/com/realestate/controller/DistrictController.java
+++ src/main/java/com/realestate/controller/DistrictController.java
@@ -19,7 +19,7 @@ public class DistrictController {
     private final DistrictService districtService;
 
     @GetMapping
-    public ResponseEntity<List<District>> getCountry(){
+    public ResponseEntity<List<District>> getDistrict(){
         return districtService.getDistrict();
     }
 
